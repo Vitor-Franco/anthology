9be4b759c01c72700f0e1a58977cbf327148d088
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-20 17:52:31 -0300

    add button stories

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from './button';
+
+const meta = {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Button',
+    disabled: false,
+    size: 'medium',
+  },
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
+    variant: {
+      control: 'select',
+    },
+  },
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+  },
+};
